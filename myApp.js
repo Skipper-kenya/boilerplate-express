e815668f449f3ac855d3e1ceb848ca110dcb6dc3
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-03-02 05:55:12 +0000

    lectures

--- myApp.js
+++ myApp.js
@@ -1,9 +1,28 @@
 let express = require('express');
 let app = express();
+let dotenv = require('dotenv');
+
+dotenv.config()
+
+app.use("/public", express.static(__dirname + "/public"));
+
+
+app.get('/json',(req,res)=>{
+    if(process.env.MESSAGE_STYLE==="uppercase"){
+        res.json({"message":"HELLO JSON"})
+    }else{
+         res.json({"message": "Hello json"}) 
+    }
+
+
+})
 
 
 
 
+app.get('/',(req,res)=>{
+    return res.sendFile(__dirname + '/views/index.html')
+})
 
 
 
